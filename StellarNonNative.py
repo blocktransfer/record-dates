1af4960d5df10efd2f3b1d7be8cc04feba57478b
John Wooten <jwooten31@gatech.edu>
2021-08-16 17:41:49 -0400

    Update StellarNonNative.py

--- StellarNonNative.py
+++ StellarNonNative.py
@@ -9,34 +9,37 @@ searchLimitMax200 = '200'
 HorizonInstance = 'horizon.stellar.org'
 
 def StellarNonNative(queryAsset, issuerAddress, desiredDateISO8601):
-  recordDateBlockHeight = getFirstBlockHeightAfterOrEqualToDate(desiredDateISO8601)
+    getAssetholdersBalancesNow(queryAsset, issuerAddress)
   
+    # Step 2 : Get all the transfers back `till the record date
+    recordDateBlockHeight = getFirstBlockHeightAfterOrEqualToDate(desiredDateISO8601)
+    #
+    
+    
+    
+    #
+
+
+    # Step 3 : Update the balances from #1 based on #2
+
   
-  # Step 1 : Get all the assetholders' balances today 
-  
-  # Step 2 : Get all the transfers back `till the record date
-  
-  # Step 3 : Update the balances from #1 based on #2
-  
-  
-  # From mergeBlockchainBalancesWithMSF in stellar-interface -> issuers : 
-  StellarBlockchainBalances = {}
-  requestAddress = 'https://' + HorizonInstance + '/accounts?asset=' + queryAsset + ':' + issuerAddress + '&limit=' + searchLimitMax200
-  data = requests.get(requestAddress).json()
-  blockchainRecords = data['_embedded']['records']
-  while(blockchainRecords != []):
-    for accounts in blockchainRecords:
-      accountAddress = accounts['id']
-      for balances in accounts['balances']:
-        if balances['asset_type'] != 'native' and balances['asset_code'] == queryAsset:
-          accountBalance = float(balances['balance'])
-          break
-      StellarBlockchainBalances[accountAddress] = accountBalance
-    # Go to next cursor
-    requestAddress = data['_links']['next']['href'].replace('%3A', ':')
+def getAssetholdersBalancesNow(queryAsset, issuerAddress):
+    StellarBlockchainBalances = {}
+    requestAddress = 'https://' + HorizonInstance + '/accounts?asset=' + queryAsset + ':' + issuerAddress + '&limit=' + searchLimitMax200
     data = requests.get(requestAddress).json()
     blockchainRecords = data['_embedded']['records']
-  return StellarBlockchainBalances
+    while(blockchainRecords != []):
+        for accounts in blockchainRecords:
+            accountAddress = accounts['id']
+            for balances in accounts['balances']:
+                if balances['asset_type'] != 'native' and balances['asset_code'] == queryAsset:
+                    accountBalance = float(balances['balance'])                                       # TODO: Run some test cases on float exports
+                    break
+            StellarBlockchainBalances[accountAddress] = accountBalance
+        # Go to next cursor
+        requestAddress = data['_links']['next']['href'].replace('%3A', ':')
+        data = requests.get(requestAddress).json()
+        blockchainRecords = data['_embedded']['records']
 
 # Simple decriment + re-increment. To improve, we reccomend merge sort
 def getFirstBlockHeightAfterOrEqualToDate(desiredDateISO8601): # '20XX-MM-DDT00:00:00Z' ++record date
