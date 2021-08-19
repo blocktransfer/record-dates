7982743e0f4b45f55debe4ad2ac6340be269b356
John Wooten <jwooten31@gatech.edu>
2021-08-19 09:51:00 -0400

    Update StellarNonNative.py

--- StellarNonNative.py
+++ StellarNonNative.py
@@ -1,3 +1,17 @@
+# This breaks in environments with extremely high throughput,
+# namely wherein shareholders transfer assets so often that balances
+# change from the runtime of getting the current sequence number and
+# getting the assetHolderBalances. This means assetholders who transfer
+# shares between the randomly-timed execution of this script finding
+# runtimeSequenceNumber and getAssetholderBalancesNow (estimated at a
+# few seconds) could incidentally receive less shares on record, while
+# the recipient receives more. This is a major issue past 200 holders, 
+# and the only way to rectify it is to manually go back and check 
+# transfers during the scripting period afterwards, ensuring no 
+# misappropriations of dividends or voting rights post-call.
+# To fix: impliment via direct ledger database records rather than API
+# or uncover a different direct-ledger query to bypass txn simulation.
+    
 import requests
 import json
 import dateutil.parser
@@ -6,20 +20,6 @@ searchLimitMax200 = '200'
 HorizonInstance = 'horizon.stellar.org'
 
 def StellarNonNative(queryAsset, issuerAddress, desiredDateISO8601): # '20XX-MM-DDT00:00:00Z'
-    # This breaks in environments with extremely high throughput,
-    # namely wherein shareholders transfer assets so often that balances
-    # change from the runtime of getting the current sequence number and
-    # getting the assetHolderBalances. This means assetholders who transfer
-    # shares between the randomly-timed execution of this script finding
-    # runtimeSequenceNumber and getAssetholderBalancesNow (estimated at a
-    # few seconds) could incidentally receive less shares on record, while
-    # the recipient receives more. This is a major issue past 200 holders, 
-    # and the only way to rectify it is to manually go back and check 
-    # transfers during the scripting period afterwards, ensuring no 
-    # misappropriations of dividends or voting rights post-call.
-    # To fix: impliment via direct ledger database records rather than API
-    # or uncover a different direct-ledger query to bypass txn simulation.
-    
     runtimeSequenceNumber = requests.get('https://' + HorizonInstance).json()['core_latest_ledger']
     assetholderBalancesNow = getAssetholderBalancesNow(queryAsset, issuerAddress)
     recordDateBlockHeight = getFirstBlockHeightAfterOrEqualToDate(desiredDateISO8601)
@@ -46,7 +46,7 @@ def getAssetholderBalancesNow(queryAsset, issuerAddress):
         blockchainRecords = data['_embedded']['records']
     return StellarBlockchainBalances
 
-# Simple decriment + re-increment. To improve, we reccomend merge sort. Med. priority if API calls maintained
+# Decriment + re-increment. To improve, we reccomend merge sort. Med. priority if API calls maintained
 def getFirstBlockHeightAfterOrEqualToDate(desiredDateISO8601): # ++record date (first allowable block @ 00:00:00Z of next day === EoD UST)
     currentBlockHeight = requests.get('https://' + HorizonInstance).json()['core_latest_ledger']
     testSequenceNum = currentBlockHeight - 20000
@@ -80,7 +80,7 @@ def getFirstBlockHeightAfterOrEqualToDate(desiredDateISO8601): # ++record date (
         else: break
     print(blockTimeISO8601)
     return testSequenceNum
-    
+
 def updateAssetholderBalancesBasedOnTransfersAfterRecordDate(queryAsset, issuerAddress, assetholderBalancesNow, runtimeSequenceNumber, recordDateBlockHeight):
     while(recordDateBlockHeight <= runtimeSequenceNumber):
         # get recordDateBlockHeight ledger
@@ -88,7 +88,7 @@ def updateAssetholderBalancesBasedOnTransfersAfterRecordDate(queryAsset, issuerA
         data = requests.get(requestAddress).json()
         blockPayments = data['_embedded']['records']
         while(blockPayments != []):
-            for transfer in blockPayments:                 #redundant transfer['type'] == 'payment'              # TODO: Important | Claimed balances & trades are not accounted for
+            for transfer in blockPayments:                 #type isolation deprecated                             # TODO: Important | Claimed balances & trades are not accounted for
                 if transfer['transaction_successful'] and transfer['type'] == 'payment' and transfer['asset_type'] != 'native' and transfer['asset_issuer'] == issuerAddress and transfer['asset_code'] == queryAsset:
                     assetholderBalancesNow[transfer['from']] += float(transfer['amount'])
                     assetholderBalancesNow[transfer['to']] -= float(transfer['amount'])
